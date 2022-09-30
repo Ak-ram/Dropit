407943ce6476978ea01d9b50af0feda5864cf441
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-09-30 22:17:39 +0000

    Update: index.js
    Add router to <App /> component

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react'
 import { ColorModeScript } from '@chakra-ui/react'
-import {theme} from './theme'
+import {theme} from './theme';
+import {BrowserRouter as Router} from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <ChakraProvider>
         <ColorModeScript initialColorMode='theme.config.initialColorMode'/>
-        <App />
+        <Router>
+            <App />
+        </Router>
 
     </ChakraProvider>
 );
