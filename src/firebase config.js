6a9434864c984ce92277b95fbdd1e80e91167139
Restyled.io <commits@restyled.io>
2022-09-30 22:38:18 +0000

    Restyled by prettier

--- src/firebase config.js
+++ src/firebase config.js
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
-import {getAnalytics} from "firebase/analytics";
-import {initializeApp} from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey : process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain : process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-  databaseURL : process.env.REACT_APP_FIREBASE_DATABASEURL,
-  projectId : process.env.REACT_APP_FIREBASE_PROJECTID,
-  storageBucket : process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId : process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-  appId : process.env.REACT_APP_FIREBASE_APPID,
-  measurementId : process.env.REACT_APP_FIREBASE_MEASUREMENTID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
 };
 
 // Initialize Firebase
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import './index.css';
+import "./index.css";
 
-import {ChakraProvider, ColorModeScript} from '@chakra-ui/react'
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter as Router} from 'react-router-dom'
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import App from './App';
-import {theme} from './theme';
+import App from "./App";
+import { theme } from "./theme";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <ChakraProvider>
-    <ColorModeScript initialColorMode = 'theme.config.initialColorMode' />
-    <Router><App /></Router>
-
-    </ChakraProvider>);
+  <ChakraProvider>
+    <ColorModeScript initialColorMode="theme.config.initialColorMode" />
+    <Router>
+      <App />
+    </Router>
+  </ChakraProvider>
+);
