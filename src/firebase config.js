5a90a419aa9b180aeb78ec9c95230280e0fa6e40
Restyled.io <commits@restyled.io>
2022-09-30 23:44:51 +0000

    Restyled by clang-format

--- src/firebase config.js
+++ src/firebase config.js
@@ -1,14 +1,14 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import {initializeApp} from "firebase/app";
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_FIREBASE_APPID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
+  apiKey : process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain : process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  databaseURL : process.env.REACT_APP_FIREBASE_DATABASEURL,
+  projectId : process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket : process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId : process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId : process.env.REACT_APP_FIREBASE_APPID,
+  measurementId : process.env.REACT_APP_FIREBASE_MEASUREMENTID
 };
 
 // Initialize Firebase
