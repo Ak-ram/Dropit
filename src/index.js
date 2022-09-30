7621a98857067d191b2dfb66d577c8ebb3a9ba9c
Restyled.io <commits@restyled.io>
2022-09-30 22:38:16 +0000

    Restyled by clang-format

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
+import './index.css';
+
+import {ChakraProvider, ColorModeScript} from '@chakra-ui/react'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { ChakraProvider } from '@chakra-ui/react'
-import { ColorModeScript } from '@chakra-ui/react'
-import {theme} from './theme';
 import {BrowserRouter as Router} from 'react-router-dom'
+
+import App from './App';
+import {theme} from './theme';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <ChakraProvider>
-        <ColorModeScript initialColorMode='theme.config.initialColorMode'/>
-        <Router>
-            <App />
-        </Router>
+    <ColorModeScript initialColorMode = 'theme.config.initialColorMode' />
+    <Router><App /></Router>
 
-    </ChakraProvider>
-);
+    </ChakraProvider>);
