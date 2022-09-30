624453bca76768f25b96fb01769280019ce47fc9
Restyled.io <commits@restyled.io>
2022-09-30 21:41:35 +0000

    Restyled by prettier

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-import './index.css';
+import "./index.css";
 
-import {ChakraProvider, ColorModeScript} from '@chakra-ui/react'
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App';
-import {theme} from './theme'
+import App from "./App";
+import { theme } from "./theme";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <ChakraProvider>
-        <ColorModeScript initialColorMode='theme.config.initialColorMode'/>
-        <App />
-
-    </ChakraProvider>
+  <ChakraProvider>
+    <ColorModeScript initialColorMode="theme.config.initialColorMode" />
+    <App />
+  </ChakraProvider>
 );
-
