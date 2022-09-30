cddf2112ec672cb81823c2ee16cde246be2f8274
Restyled.io <commits@restyled.io>
2022-09-30 21:14:36 +0000

    Restyled by prettier

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import './index.css';
+import "./index.css";
 
-import {ChakraProvider} from '@chakra-ui/react'
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App';
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
 );
-
