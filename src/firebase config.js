d5fa4517592c1fd2b50bd3391666c2c052380952
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-09-30 22:13:27 +0000

    Update firebase config.js

--- src/firebase config.js
+++ src/firebase config.js
@@ -6,14 +6,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAe1NdocHmbz3Puji-wOtKnY7gbkqwHYcg",
-  authDomain: "dropit-945fd.firebaseapp.com",
-  databaseURL: "https://dropit-945fd-default-rtdb.firebaseio.com",
-  projectId: "dropit-945fd",
-  storageBucket: "dropit-945fd.appspot.com",
-  messagingSenderId: "366260824613",
-  appId: "1:366260824613:web:563571a569f9ffbd476dcc",
-  measurementId: "G-S5FX4BRD2D"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
 };
 
 // Initialize Firebase
