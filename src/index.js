435be1886a83cd87e443efb6b22d2ce991cd7971
Restyled.io <commits@restyled.io>
2022-09-30 21:40:57 +0000

    Restyled by clang-format

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+import './index.css';
+
+import {ChakraProvider, ColorModeScript} from '@chakra-ui/react'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
-import { ChakraProvider } from '@chakra-ui/react'
-import { ColorModeScript } from '@chakra-ui/react'
 import {theme} from './theme'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <ChakraProvider>
