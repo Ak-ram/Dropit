fccd48a0d3e821c149de9750c10fc65f722290c1
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-10-01 17:47:45 +0000

    Set: Home Route Integration

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,38 @@
+import { Flex } from "@chakra-ui/react";
 import React from "react";
+// import { Flex } from "@chakra-ui/react";
+import { Category, NavBar, VideoPin,Feed,Create,Search } from "../components";
+import { Routes,Route } from "react-router-dom";
 
 const Home = ({ user }) => {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <NavBar user={user} />
+      <Flex
+        direction={"column"}
+        justifyContent={"start"}
+        alignItems={"center"}
+        width={"20"}
+      >
+        <Category />
+      </Flex>
+
+      <Flex
+        px={"4"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        width={"full"}
+      >
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/category/:categoryId" element={<Category />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/videoDetail/:videoId" element={<VideoPin />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </Flex>
+    </>
+  );
 };
 
 export default Home;
