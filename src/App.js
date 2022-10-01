e8620e16a882e4dc7082006cba64e0a294e1b2c2
Restyled.io <commits@restyled.io>
2022-10-01 17:49:14 +0000

    Restyled by clang-format

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import "./App.css";
+
+import {useEffect, useState} from "react";
+import {Route, Routes, useNavigate} from "react-router-dom";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import { Route, Routes, useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { fetchUser, userAccessToken } from "./utils/fetchUser";
+import {fetchUser, userAccessToken} from "./utils/fetchUser";
+
 function App() {
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
   useEffect(() => {
     const accessToken = userAccessToken();
     if (!accessToken) {
-      navigate("/login", { replace: true });
+      navigate("/login", {replace : true});
     } else {
       const [userInfo] = fetchUser();
       setUser(userInfo);
