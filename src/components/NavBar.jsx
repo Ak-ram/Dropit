6715500b5ec35edc7ba8a2d3149f12a46b6733b5
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-10-02 08:37:47 +0000

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
+import logo from "../img/logo.png";
+import logo_dark from "../img/logo_dark.png";
+import { Link, useNavigate } from "react-router-dom";
+import { Flex, useColorMode, useColorModeValue } from "@chakra-ui/react";
+const NavBar = ({ user }) => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const bg = useColorModeValue("gray.600", "gray.300");
+  return (
+    <Flex
+      justifyContent={"space-between"}
+      width={"100vw"}
+      alignItems={"center"}
+      p={"4"}
+    ></Flex>
+  );
+};
 
-const NavBar = ()=>{
-    return <div>NavBar</div>
-}
-
-export default NavBar
+export default NavBar;
